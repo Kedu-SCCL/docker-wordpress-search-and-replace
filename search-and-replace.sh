9f1f0ab7493b98ae2a9bf697fe2839a71f338e57
javier.legido <javier.legido@kedu.coop>
2021-04-22 12:06:44 +0200

    Using dynamic names for both, app and db docker container, thus allowing concurrent execution of same docker-compose file, useful for CI such as Jenkins

--- search-and-replace.sh
+++ search-and-replace.sh
@@ -4,7 +4,7 @@ set -e
 
 function num_server_started()
 {
-    echo `docker logs $DB_NAME 2>&1 | grep -c "$SERVER_STARTED_LOG"`
+    echo `docker logs $GENERATED_DB_NAME 2>&1 | grep -c "$SERVER_STARTED_LOG"`
 }
 
 function add_use_database()
@@ -24,21 +24,30 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-docker-compose down
+# Let's get the timestamp in nanoseconds to allow concurrent executions, for instance when executed from Continous Integration (CI)
+TIMESTAMP=`date +%s%N`
+
+# DB container name relies on docker-compose.yml file, currently 'db'
+GENERATED_DB_NAME=$TIMESTAMP"_"$DB_NAME"_1"
+
+# App container name relies on docker-compose.yml file, currently 'app'
+GENERATED_APP_NAME=$TIMESTAMP"_"$APP_NAME"_1"
+
+docker-compose -p $TIMESTAMP down
 
 add_use_database
 
-docker-compose up -d --build --force-recreate
+docker-compose -p $TIMESTAMP up -d --build --force-recreate
 
 while [ `num_server_started` -lt 2 ]; do
     echo 'Waiting on database server is started up. Sleeping '$SLEEP's...'
     sleep $SLEEP
 done
 
-docker exec -i $APP_NAME php /wordpress-search-and-replace/srdb.cli.php -h $DB_NAME -n $DB_MYSQL_DATABASE -u $DB_MYSQL_USER -p $DB_MYSQL_PASSWORD -s $1 -r $2
+docker exec -i $GENERATED_APP_NAME php /wordpress-search-and-replace/srdb.cli.php -h $GENERATED_DB_NAME -n $DB_MYSQL_DATABASE -u $DB_MYSQL_USER -p $DB_MYSQL_PASSWORD -s $1 -r $2
 
 rm -fr $OUTPUT_FILE
 
-docker exec -i $DB_NAME mysqldump --extended-insert=FALSE $DB_MYSQL_DATABASE -u $DB_MYSQL_USER -p$DB_MYSQL_PASSWORD > $OUTPUT_FILE
+docker exec -i $GENERATED_DB_NAME mysqldump --extended-insert=FALSE $DB_MYSQL_DATABASE -u $DB_MYSQL_USER -p$DB_MYSQL_PASSWORD > $OUTPUT_FILE
 
-docker-compose down
+docker-compose -p $TIMESTAMP down
